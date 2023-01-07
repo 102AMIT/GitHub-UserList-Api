fc0b45d8d65baceed4217c6425814e62cfce2858
Amit Kumar Thakur <kingston.amit9830@gmail.com>
2023-01-08 02:49:52 +0530

    creting different components

---
+++ src/Components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='loading'>
+        <h1 style={{fontSize:"45px"}}>Loading...</h1>
+    </div>
+  )
+}
+
+export default Loading
